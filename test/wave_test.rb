6be1e13b9f932c0d68b95b6b81f670d1affcac6c
Jack Danger Canty <gitcommit@6brand.com>
2009-05-29 19:20:36 -0700

    writing basic Wave instantiaion tests

--- test/wave_test.rb
+++ test/wave_test.rb
@@ -1,8 +1,18 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class WaveTest < Test::Unit::TestCase
-  should "like, at least, y'know, basically compile" do
-    assert Wave::VERSION
+
+  context "instantiating a wave" do
+    context "without any id" do
+      should "raise an error" do
+        assert_raises(ArgumentError) { @wave = Wave.new }
+      end
+    end
+    context "without a valid id" do
+      should "raise an error" do
+        assert_raises(Wave::WaveError) { @wave = Wave.new('') }
+      end
+    end
   end
 end
 
