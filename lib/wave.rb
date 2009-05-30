b1b70f6e1e3aec210f4dd2581ef154db91d641c7
Jack Danger Canty <gitcommit@6brand.com>
2009-05-29 19:20:22 -0700

    adding Wave class with basic description

--- lib/wave.rb
+++ lib/wave.rb
@@ -1,10 +1,30 @@
-module Wave
+# Wave
+#
+#  A wave is identified by a globally unique wave id, which is
+#  a pair of a domain name and an id string. The domain names
+#  the wave provider where the wave originated.
+#  So if you're accessing a wave initiated by wave.google.com
+#  the Wave id may look like the following:
+#
+#    a12f4d@wave.google.com
+#
+#  A wave contains a collection of wavelets which each have a
+#  distinct list of participants, documents, and history (operations)
+
+
+class Wave
   VERSION = File.read(File.dirname(__FILE__) + "/../VERSION").chomp
+
+  class WaveError < StandardError; end
+
+  def initialize(id, options = {})
+    raise WaveError,
+          "provide a Wave id in the format: guid@domain.com" unless
+              id.to_s =~ /^[\w\d]+@[\w\d]+\.[\w\d]+/
+  end
 end
 
 
-# $:.unshift File.dirname(__FILE__)
-
-p $:
+$:.unshift File.dirname(__FILE__)
 
 require "wave/robot"
